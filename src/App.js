314efea85c327d7400cf3273dbaacd6259fafa7a
JON J <gr8fuldwg@gmail.com>
2018-11-15 19:06:01 -0500

    added chuck pic to main UI

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 // import Modal from 'react-modal';
 import './App.css';
 // import axios from 'axios';
+import chuckPic from './chuck_pic.png';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        Chuck Jokes!
+      <div className="vh-100 bg-purple flex flex-column justify-center items-center">
+        <button ></button>
+        <img src={chuckPic} />
       </div>
+
     );
   }
 }
