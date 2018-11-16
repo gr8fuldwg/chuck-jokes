0ef13cf5486688734a915cc74c21c0abdcf59a20
JON J <gr8fuldwg@gmail.com>
2018-11-15 19:33:49 -0500

    created button

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div className="vh-100 bg-purple flex flex-column justify-center items-center">
-        <button ></button>
+        <button className="flex justify-center items-center f1  outline-black br-100 h5 w5 dim ph8 pv2 dib black bg-yellow ">Hit Me!</button>
         <img src={chuckPic} />
       </div>
 
