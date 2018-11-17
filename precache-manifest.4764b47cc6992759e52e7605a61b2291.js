2de974c16e9918e02b8bfe4b701b0cbc96607b43
JON J <gr8fuldwg@gmail.com>
2018-11-16 19:40:51 -0500

    Updates

---
+++ precache-manifest.4764b47cc6992759e52e7605a61b2291.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "39b94c0e41581c1258965096f71484c9",
+    "url": "/chuck-jokes/static/media/chuck_pic.39b94c0e.png"
+  },
+  {
+    "revision": "afc79ad6bf1ecbf64b10",
+    "url": "/chuck-jokes/static/js/runtime~main.afc79ad6.js"
+  },
+  {
+    "revision": "dc275ba1de89be2478c6",
+    "url": "/chuck-jokes/static/js/main.dc275ba1.chunk.js"
+  },
+  {
+    "revision": "30b94a413199f6bbee8f",
+    "url": "/chuck-jokes/static/js/1.30b94a41.chunk.js"
+  },
+  {
+    "revision": "dc275ba1de89be2478c6",
+    "url": "/chuck-jokes/static/css/main.0b16c7dc.chunk.css"
+  },
+  {
+    "revision": "30b94a413199f6bbee8f",
+    "url": "/chuck-jokes/static/css/1.91952151.chunk.css"
+  },
+  {
+    "revision": "fad255474c3b9a4b47d4f546d371800b",
+    "url": "/chuck-jokes/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/chuck-jokes/precache-manifest.4764b47cc6992759e52e7605a61b2291.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/chuck-jokes/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
